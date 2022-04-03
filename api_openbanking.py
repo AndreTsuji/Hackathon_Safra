35ab9e0988fe1fc3ce0db361c66e29d8a6c35342
AndreTsuji <tsuji.matins@gmail.comi>
2022-04-03 11:03:14 -0300

    Desenvolvimento das rotinas de Login, cadastro, simulação de crédito, e tratativas de exceção

--- api_openbanking.py
+++ api_openbanking.py
@@ -1,3 +1,6 @@
+import cProfile
+
+
 class Openbanking:
 
     def consulta_api():
@@ -42,3 +45,73 @@ class Openbanking:
         return lista_servicos
 
     # Desenvolver def de consulta de dados de clientes (necessita credenciais do Banco Safra + autorização do cliente)
+    # def info_cliente():
+
+
+class Rotinas:
+
+    # Função retorna resultados do layout de simulação.
+    def simulador_juros(credito, entrada, periodo, juros):
+        # simulador = [montante, parcela, total de juros]
+        montante = credito * ((1+juros)**periodo)
+        simulador = [montante, montante/periodo, montante - credito]
+
+        return simulador
+
+     # Função login
+    def login(login, senha):
+        import mysql.connector
+
+        conexao = mysql.connector.connect(
+            host='localhost', database='db_Cadastro', user='root', password='')
+
+        if conexao.is_connected():
+            db_info = conexao.get_server_info()
+            cursor = conexao.cursor()
+            cursor.execute("select ")
+
+        # Desativa a conexão com a base de dados após uso.
+        if conexao.is_connected():
+            cursor.close()
+            conexao.close()
+
+        return(dados_cliente)
+            
+    # Função adiciona cadastro de novo usuário em base de dados
+    def cadastro():
+        import mysql.connector
+        from mysql.connector import Error
+        from datetime import datetime
+
+        # Com os campos de entradas de dados já referenciados, atribuir diretamento os valores
+        tbl_nome = input("Nome: ")
+        tbl_cpf = input("CPF: ")
+        tbl_senha = input("Senha: ")
+        tbl_nasc = input("Data de nascimento: ")
+        tbl_email = input("E-mail: ")
+        tbl_telefone = input("Telefone: ")
+        tbl_setor = input("Setor: ")
+        tbl_autorizacao_openbanking = datetime.today().strftime('%d/%m/%Y')
+
+        # Definição da query SQL para adicionar o novo cliente na base cadastral.
+        # Junto com o cadastro será solicitado a autorizacao de consulta dos dados cliente no OpenBanking durante 6 meses.
+        # A variável autorização_openbanking registrará a data do cadastro e servirá de flag para alertar o Safra o momento
+        # que terá venciodo o periodo de autorização de uso dos dados do OpenBanking.
+        declaracao = """INSERT INTO tbl_user (nome, cpf, senha, nasc, autorizacao_openbanking, email, telefone, setor) VALUES ("""
+        dados = "'" + tbl_nome + "','" + tbl_cpf + "','" + tbl_senha + "','" + tbl_nasc + "','" + \
+            str(tbl_autorizacao_openbanking) + "','" + \
+            tbl_email + "','" + tbl_telefone + "','" + tbl_setor + "')"
+        comando = declaracao + dados
+        try:
+            conexao = mysql.connector.connect(
+                host='localhost', database='db_Cadastro', user='root', password='')
+
+            inserir_cadastro = comando
+            cursor = conexao.cursor()
+            cursor.execute(inserir_cadastro)
+
+            # Desativa a conexão com a base de dados após uso.
+            conexao.commit()
+            cursor.close()
+        except Error as erro:
+            print("Falha no processo de cadastro.")
